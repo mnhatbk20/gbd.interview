f5bfc3d383c9c1998a3b30ded37d5ca01dfc8c03
mnhatbk20 <mnhatbk20@gmail.com>
2022-11-23 20:46:33 +0700

    nhat

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 define('APP_ROOT', true);
 
@@ -6,35 +6,40 @@ const GET = "GET";
 const POST = "POST";
 const PUSH = "PUSH";
 
-function getMethod(){
+function getMethod()
+{
     return $_SERVER["REQUEST_METHOD"];
 }
 
-function getPath(){
+function getPath()
+{
     $path = $_SERVER['REQUEST_URI'];
     $position = strpos($path, '?');
     if ($position !== false) {
         $path = substr($path, 0, $position);
-    }       
-    str_ends_with($path,"/")?$path:($path."/");
+    }
+    str_ends_with($path, "/") ? $path : ($path . "/");
     return $path;
 }
 
-function runController($nameController){
+function runController($nameController)
+{
     $ROOTPATH = __DIR__;
-    $pathController = $ROOTPATH."\\controllers\\".$nameController.".php";
+    $pathController = $ROOTPATH . "\\controllers\\" . $nameController . ".php";
     require_once($pathController);
     die();
 }
 
-function showView($nameView,$dataView=null){
+function showView($nameView, $dataView = null)
+{
     $ROOTPATH = __DIR__;
     $data = $dataView;
-    $pathView = $ROOTPATH."\\views\\".$nameView.".php";
+    $pathView = $ROOTPATH . "\\views\\" . $nameView . ".php";
     require_once($pathView);
 }
 
-function responseJson($data){
+function responseJson($data)
+{
     ob_clean();
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode($data);
@@ -42,16 +47,17 @@ function responseJson($data){
 }
 
 
-function runApp(){
+function runApp()
+{
 
     $method = getMethod();
-    $path = getPath();  
+    $path = getPath();
 
-    if ($method == GET){    
-        require_once("routers/get.php");        
+    if ($method == GET) {
+        require_once("routers/get.php");
     }
-    if ($method == POST){
-        require_once("routers/post.php");   
+    if ($method == POST) {
+        require_once("routers/post.php");
     }
 };
 
